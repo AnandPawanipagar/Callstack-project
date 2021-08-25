f545572e4d7ffd963318ee90e51ede076607d1d0
AnandPawanipagar <85126312+AnandPawanipagar@users.noreply.github.com>
2021-08-25 17:36:20 +0530

    poping is also done

--- callstack.js
+++ callstack.js
@@ -2,7 +2,6 @@ var count = "0";
 
 function pushElement() {
   count++;
-  console.log(count);
   var block_name = "";
   var highcount = count;
   for (var i = 0; i < count; i++) {
@@ -12,8 +11,25 @@ function pushElement() {
   }
   console.log(block_name);
   document.getElementById("boxes").innerHTML = block_name;
+  console.log(document.getElementById('name').value)
 }
 
 function popElement() {
  
+  if(count!=0)
+  {
+    count--;
+  }
+  console.log(count)
+  document.getElementById("boxes").innerHTML = '';
+  var highcount = count;
+  var block_name='';
+  for (var i = 0; i < count; i++) {
+    highcount--;
+    block_name =
+      block_name + '<div class="Box">' + Number(highcount + 1) + "</div>";
+  }
+  console.log(block_name);
+  document.getElementById("boxes").innerHTML = block_name;
 }
+
